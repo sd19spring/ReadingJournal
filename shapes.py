f1f89fac5fde13a858f4abd1c0a475fb64b1fe86
Benjamin Hill <benjamin.hill@olin.edu>
2019-02-08 06:54:37 -0500

    Add Day 5 reading journal exercises
    Includes supplemental file `shapes.py` for running turtle exercises

---
+++ shapes.py
@@ -0,0 +1,46 @@
+import turtle
+
+#------------------------------------------------------------------------------
+# Starter code to get things going
+# (feel free to delete once you've written your own functions
+#------------------------------------------------------------------------------
+
+# Create the world, and a turtle to put in it
+bob = turtle.Turtle()
+
+# Get moving, turtle!
+bob.fd(100)
+
+# Wait for the user to close the window
+turtle.mainloop()
+
+
+#------------------------------------------------------------------------------
+# Make some shapes
+#   Work through exercises 1-4 in Chapter 4.3.
+#------------------------------------------------------------------------------
+
+# NOTE: for part 2 of 4.3, you will add another parameter to this function
+def square(t):
+    """
+    Draw a square using Turtle t
+    
+    >>> don = turtle.Turtle()
+    >>> square(don)
+    """
+    pass # Delete this line and replace it with your code
+
+
+## Polygon
+
+
+## Circle
+
+
+#------------------------------------------------------------------------------
+# Make some art
+#   Complete *at least one of* Exercise 2, 3, 4, or 5 in `shapes.py`. 
+#------------------------------------------------------------------------------
+
+# If you come up with some cool drawings you'd like to share with the rest of the class, let us know!
+
